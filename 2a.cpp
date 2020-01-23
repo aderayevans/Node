5b7bfd59312b140ae8a40ec39ab2c64f5b2d7ea4
unknown <aderayevans@gmail.com>
2020-01-23 22:53:34 +0700

    finished 4

--- 2a.cpp
+++ 2a.cpp
@@ -18,11 +18,22 @@ void ARRANGE_LIST(List &L) {
         second = first->Next;
     }
 }
+void ADD_LIST(ElementType x, List &L) {
+    ARRANGE_LIST(L);
+    Position p = L;
+    while (p->Next != NULL) {
+        if (p->Next->Element > x) {
+            break;
+        }
+        else p = p->Next;
+    }
+    INSERT_LIST(x, p, L);
+}
 int main() {
     List L;
     MAKENULL_LIST(L);
     READ_LIST(L);
     PRINT_LIST(L);
-    ARRANGE_LIST(L);
+    ADD_LIST(40, L);
     PRINT_LIST(L);
 }
