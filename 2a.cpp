427a68f66b74681eb4ccbda3b67899d2f82af07a
unknown <aderayevans@gmail.com>
2020-01-23 21:31:29 +0700

    finished 3

--- 2a.cpp
+++ 2a.cpp
@@ -1,13 +1,28 @@
 #include <iostream>
 #include "LIST2a.h"
 
+void ARRANGE_LIST(List &L) {
+    ElementType temp;
+    Position first = L;
+    Position second = L->Next;
+    while (first->Next != NULL) {
+        while (second->Next != NULL) {
+            if (first->Next->Element > second->Next->Element) {
+                temp = first->Next->Element;
+                first->Next->Element = second->Next->Element;
+                second->Next->Element = temp;
+            }
+            second = second->Next;
+        }
+        first = first->Next;
+        second = first->Next;
+    }
+}
 int main() {
     List L;
     MAKENULL_LIST(L);
     READ_LIST(L);
     PRINT_LIST(L);
-    std::cout << "Phan tu " << RETRIEVE(LOCATE(5, L), L);
-    std::cout << "\nDeleting..\n";
-    DELETE_LIST(LOCATE(8, L), L);
+    ARRANGE_LIST(L);
     PRINT_LIST(L);
 }
