4374f9e8ed3bd263f13327d412ee0c40a7e58d00
unknown <aderayevans@gmail.com>
2020-01-24 18:05:33 +0700

    done 8

--- 2a.cpp
+++ 2a.cpp
@@ -19,7 +19,6 @@ void ARRANGE_LIST(List &L) {
     }
 }
 void ADD_LIST(ElementType x, List &L) {
-    ARRANGE_LIST(L);
     Position p = L;
     while (p->Next != NULL) {
         if (p->Next->Element > x) {
@@ -29,11 +28,76 @@ void ADD_LIST(ElementType x, List &L) {
     }
     INSERT_LIST(x, p, L);
 }
+void ADVANCED_ADD_LIST(ElementType x, List &L) {
+    Position p = L;
+    bool stop_signal = false;
+    while (p->Next != NULL) {
+        if (p->Next->Element == x) {
+            stop_signal = true;
+            break;
+        }
+        else if (p->Next->Element > x) {
+            break;
+        }
+        else p = p->Next;
+    }
+    if (!stop_signal) INSERT_LIST(x, p, L);
+}
+void DEL_ELEMENT(ElementType x, List &L) {
+    Position p = L;
+    while (p->Next != NULL) {
+        if (p->Next->Element == x) {
+            DELETE_LIST(p, L);
+            break;
+        }
+        else p = p->Next;
+    }
+}
+void INPUT_LIST(List &L) {
+    int amount;
+    ElementType x;
+    std::cout << "List amount: ";
+    std::cin >> amount;
+    for (int index = 0; index < amount; index++) {
+        std::cout << "/<" << index + 1 << ">: ";
+        std::cin >> x;
+        ADD_LIST(x, L);
+    }
+}
+void DEL_DUP(List &L) {
+    Position first = L;
+    Position second = L->Next;
+    while (first->Next != NULL) {
+        while (second->Next != NULL) {
+            if (first->Next->Element == second->Next->Element) {
+                DELETE_LIST(second, L);
+            }
+            second = second->Next;
+        }
+        first = first->Next;
+        second = first->Next;
+    }
+}
+void ADVANCED_INPUT_LIST(List &L) {
+    int amount;
+    ElementType x;
+    std::cout << "List amount: ";
+    std::cin >> amount;
+    for (int index = 0; index < amount; index++) {
+        std::cout << "/<" << index + 1 << ">: ";
+        std::cin >> x;
+        ADVANCED_ADD_LIST(x, L);
+    }
+}
 int main() {
     List L;
     MAKENULL_LIST(L);
-    READ_LIST(L);
+//    INPUT_LIST(L);
+    ADVANCED_INPUT_LIST(L);
     PRINT_LIST(L);
     ADD_LIST(40, L);
+    PRINT_LIST(L);
+//    DEL_ELEMENT(40, L);
+    DEL_DUP(L);
     PRINT_LIST(L);
 }
