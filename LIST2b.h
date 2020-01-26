3039f5b0f0f72b78d2e104f0aa54ca0ddaa3f4af
unknown <aderayevans@gmail.com>
2020-01-26 10:26:19 +0700

    reuse LIST2b, done 12d

--- LIST2b.h
+++ LIST2b.h
@@ -1,84 +1,84 @@
-typedef int ElementType;
-struct Node {
-    ElementType Element; //contains Element of the previous Node
-    Node* Next; //contains address of the next Node
+typedef int intElementType;
+struct intNode {
+    intElementType Element; //contains Element of the previous intNode
+    intNode* Next; //contains address of the next intNode
 };
-typedef Node* Position; //assign Node address to Position pointer
-//List contains Header-Node address
-typedef Position List; //In essence, when we declare a list, we are declare a Header Node
-void INSERT_LIST(ElementType x, Position p, List &L) {
-    Position t = new Node(); //declare and allocate a Node to contain x
-    t->Element = x; //put x to the Node, in essence that x is p's element
+typedef intNode* intPosition; //assign intNode address to intPosition pointer
+//intList contains Header-intNode address
+typedef intPosition intList; //In essence, when we declare a intList, we are declare a Header intNode
+void INSERT_intList(intElementType x, intPosition p, intList &L) {
+    intPosition t = new intNode(); //declare and allocate a intNode to contain x
+    t->Element = x; //put x to the intNode, in essence that x is p's element
     t->Next = p->Next; //assign (p+1)'s address to t->Next which is containing in p->Next
-    p->Next = t; //assign t (a Node address) to p->Next
+    p->Next = t; //assign t (a intNode address) to p->Next
 }
-Position LOCATE(ElementType x, List L) {
-    //Now we see L as a position
+intPosition LOCATE(intElementType x, intList L) {
+    //Now we see L as a intPosition
     while (L->Next != NULL)
         if (L->Next->Element == x) return L;
         else L = L->Next;
     return L;
 }
-ElementType RETRIEVE(Position p, List L) {
+intElementType RETRIEVE(intPosition p, intList L) {
     if (p->Next != NULL)
         return p->Next->Element;
-    std::cout << "Position Not Found!!!";
+    std::cout << "intPosition Not Found!!!";
     return 0;
 }
-void DELETE_LIST(Position p, List &L) {
+void DELETE_intList(intPosition p, intList &L) {
     //In essence, we're working on, p->Next = P->Next->Next;
     //But we need to delete the pointer that contain p->Next->Element;
     if (p->Next != NULL) {
-        Position temp = p->Next;
+        intPosition temp = p->Next;
         p->Next = temp->Next;
         delete temp;
     }
 }
-Position NEXT(Position p, List L) {
+intPosition NEXT(intPosition p, intList L) {
     return p->Next;
 }
-Position PREVIOUS(Position p, List L) {
-    //See L as Header Node
+intPosition PREVIOUS(intPosition p, intList L) {
+    //See L as Header intNode
     while (L->Next != p)
         L = L->Next;
     return L;
 }
-Position FIRST(List L) {
+intPosition FIRST(intList L) {
     return L;
 }
-bool EMPTY_LIST(List L) {
+bool EMPTY_intList(intList L) {
     return L->Next == NULL;
 }
-void MAKENULL_LIST(List &L) {
+void MAKENULL_intList(intList &L) {
     //Now &L is &Header
-    L = new Node(); //allocate Header
+    L = new intNode(); //allocate Header
     L->Next = NULL;
 }
 //Extra abstract function
-Position ENDLIST(List L) {
+intPosition ENDintList(intList L) {
     while (L->Next != NULL)
         L = L->Next;
     return L;
 }
-void READ_LIST(List &L) {
+void READ_intList(intList &L) {
     int amount;
-    ElementType x;
+    intElementType x;
     std::cout << "Nhap so phan tu danh sach: ";
     std::cin >> amount;
     for (int index = 0; index < amount; index++) {
         std::cout << "/<" << index + 1 << ">: ";
         std::cin >> x;
-        INSERT_LIST(x, FIRST(L), L);
+        INSERT_intList(x, FIRST(L), L);
     }
 }
-void PRINT_LIST(List L) {
+void PRINT_intList(intList L) {
     int amount = 0;
-    Position temp = L;
+    intPosition temp = L;
     while (L->Next != NULL) {
         amount++;
         L = L->Next;
     }
-    std::cout << "List amount: " << amount << std::endl;
+    std::cout << "intList amount: " << amount << std::endl;
     for (int index = 0; index < amount; index++) {
         std::cout << "/<" << (index + 1) << ">: "
         << temp->Next->Element << std::endl;
