94a309d9377a23165ba8fc9809bf9447f5c58278
unknown <aderayevans@gmail.com>
2020-04-13 14:20:47 +0700

    fix add bignumber

--- 15.cpp
+++ 15.cpp
@@ -2,19 +2,29 @@
 #include <string>
 #include "LIST15.h"
 
+using std::cout;
+using std::endl;
+
 List ADD_BIGNumber(List L1, List L2) {
     List L;
     int remember = 0;
     MAKENULL_LIST(L);
-    while (L1->Next != NULL || L2->Next != NULL) {
-        if (L1->Next != NULL && L2->Next == NULL) {
-            INSERT_LIST((L1->Next->Element + remember)%10, FIRST(L), L);
+    while (true) {
+        if (L1->Next == NULL && L2->Next == NULL && remember == 0) {
+            break;
+        }
+        else if (L1->Next == NULL && L2->Next == NULL && remember != 0) {
+            INSERT_LIST(remember, FIRST(L), L);
             remember = 0;
+        }
+        else if (L1->Next != NULL && L2->Next == NULL) {
+            INSERT_LIST((L1->Next->Element + remember)%10, FIRST(L), L);
+            remember = (L1->Next->Element + remember)/10;
             L1 = L1->Next;
         }
         else if (L1->Next == NULL && L2->Next != NULL) {
             INSERT_LIST((L2->Next->Element + remember)%10, FIRST(L), L);
-            remember = 0;
+            remember = (L2->Next->Element + remember)/10;
             L2 = L2->Next;
         }
         else {
